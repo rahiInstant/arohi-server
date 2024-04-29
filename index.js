37b7a231d6ae18309dd1d964285a00205d57df44
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-04-29 20:09:42 +0600

    get and insert data of spot

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express')
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 3600
-const uri = `mongodb+srv://rahiurp20:C2GwroCh81l0vqmB@touring.uoxy8h0.mongodb.net/?retryWrites=true&w=majority&appName=touring`;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3600;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@touring.uoxy8h0.mongodb.net/?retryWrites=true&w=majority&appName=touring`;
 
-
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,7 +15,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,27 +24,50 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const touristDB = client.db("tourist").collection("spot");
+    // const countryCollection = touristDB.collection("country");
+    app.post("/spot", async (req, res) => {
+      const data = req.body;
+      const result = await touristDB.insertOne(data);
+      res.send(result);
+      // console.log(data)
+    });
+    app.get("/spot", async (req, res) => {
+      const query = {};
+      const option = {
+        projection: {
+          spot: 1,
+          time: 1,
+          cost: 1,
+          visitor: 1,
+          season: 1,
+          photo: 1,
+        },
+      };
+      const spotCollection = touristDB.find(query, option);
+      const result = await spotCollection.toArray();
+      res.send(result);
+      console.log(result);
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/',(req, res) => {
-    console.log('welcome to our tourist spot.')
-    res.send('welcome to our tourist spot.')
-})
+app.get("/", (req, res) => {
+  res.send("welcome to our tourist spot.");
+});
 
 app.listen(port, () => {
-    console.log(`server is running port ${port}`)
-})
-
-
+  console.log(`server is running port ${port}`);
+});
 
 // rahiurp20
 // C2GwroCh81l0vqmB
+
+// {"_id":{"$oid":"662f646c20dade7955e6c3f4"},"country":"Bangladesh","spot":"Hakaluki Romance Spot","time":{"$numberInt":"12"},"visitor":{"$numberInt":"10000"},"photo":"https://i.postimg.cc/qqCvc3Fz/card-01.jpg","location":"Bandarban,bangladesh","cost":{"$numberInt":"27000"},"season":"thailand","comment":"Nestled in Bangladesh's southeastern hills, Bandarban captivates with lush forests, tribal culture, and the majestic peaks of the Chittagong Hill Tracts. Explore its beauty through trekking, indigenous crafts, and serene landscapes.","userName":"Adbur Rahaman Rahi","userEmail":"rahiurp20@gmail.com"}
